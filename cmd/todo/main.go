69717ea1f3429b832430518fb245a48216b34259
syamimhazmi <syamimhazmi@gmail.com>
2025-03-10 16:42:52 +0700

    saved task based on given input

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	todo "github.com/syamimhazmi/go-todo-cli"
 )
@@ -27,9 +31,15 @@ func main() {
 
 	switch {
 	case *add:
-		todos.Add("Sample todo")
+		task, err := getInput(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 
-		err := todos.Store(todoFile)
+		todos.Add(task)
+
+		err = todos.Store(todoFile)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
@@ -64,4 +74,24 @@ func main() {
 		fmt.Fprintln(os.Stdout, "invalid command")
 		os.Exit(1)
 	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0 {
+		return "", errors.New("empty todo is not allowed")
+	}
+
+	return text, nil
 }
